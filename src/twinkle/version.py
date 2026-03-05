ec3a3440289753bf8ba8fd4ebc0a3be83a6d5f54
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-03-05 23:37:45 +0800

    bump version

--- src/twinkle/version.py
+++ src/twinkle/version.py
@@ -1,9 +1,7 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-<<<<<<< HEAD
-__version__ = '0.1.1'
-=======
-__version__ = '0.2.dev0'
->>>>>>> main
+
+__version__ = '0.1.2'
+
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
 __release_datetime__ = '2099-10-13 08:56:12'
